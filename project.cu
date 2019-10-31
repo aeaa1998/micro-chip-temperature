3b0b3527d5a11a907f91dc61535dff48058fab7f
Augusto Alonso <augustoalonso98@gmail.comn>
2019-10-30 23:02:22 -0600

    commit sad

--- project.cu
+++ project.cu
@@ -14,49 +14,57 @@
 
 using namespace std;
 __global__
-void getTotal(int n, float *array, float *total)
+void getTotal(int n, float *array, float * totalL)
 {
   int index = blockIdx.x*blockDim.x + threadIdx.x;
 
-  if (index < n) total += array[index];
+  if (index < n) totalL[0] += (array[index]);
+
 }
-void getVariance(int n, float *array, float *mean, float *vairance)
+__global__
+
+void getVariance(int n, float *array, float * mean, float * vairance)
 {
   int index = blockIdx.x*blockDim.x + threadIdx.x;
 
-  if (index < n) vairance += pow(array[index] - mean, 2);
+  if (index < n) vairance[0] += (pow(array[index] - mean[0], 2));
 }
 
-vector<string> explode(string const & s, char delim)
-{
-    vector<string> result;
-    istringstream iss(s);
+// vector<string> explode(string const & s, char delim)
+// {
+//     vector<string> result;
+//     istringstream iss(s);
 
-    for (string token; getline(iss, token, delim); )
-    {
-        result.push_back(move(token));
-    }
+//     for (string token; getline(iss, token, delim); )
+//     {
+//         result.push_back(move(token));
+//     }
 
-    return result;
-}
+//     return result;
+// }
 
 int main(void)
 {
 	int N = 100000;
 	float *hoursArray, *cudaHours;
-	float #temperaturesArray, *cudaTemperatures;
+	float *temperaturesArray, *cudaTemperatures;
 	hoursArray = (float*)malloc(N*sizeof(float));
 	temperaturesArray = (float*)malloc(N*sizeof(float));
 	cudaMalloc(&cudaHours, N*sizeof(float));
 	cudaMalloc(&cudaTemperatures, N*sizeof(float));
 
+	const char *ts[4] = {"temps1.txt", "temps2.txt", "temps3.txt", "temps4.txt"};
+	const char *hs[4] = {"hours1.txt", "hours2.txt", "hours3.txt", "hours4.txt"};
+	    vector<string> hoursArrayS;
+	    vector<string> tempsArrayS;
 	for (int i = 1; i < 5; ++i)
 	{
+
 		int offset = (i - 1) * 25000;
 		ifstream hoursFile;
 		ifstream tempsFile;
-		hoursFile.open("hours" + to_string(i) + ".txt");
-		tempsFile.open("temps" + to_string(i) + ".txt");
+		hoursFile.open(hs[i-1]);
+		tempsFile.open(ts[i-1]);
 		string hours = "";
 		string temps = "";
 		string line ="";
@@ -69,12 +77,24 @@ int main(void)
 	    {
 	      temps += lineT;
 	    }
-	    vector<string> hoursArrayS =explode(hours, ' ');
-	    vector<string> tempsArrayS =explode(temps, ' ');
+
+
+	    for (int i = 0; i < 3000; i+=3)
+	    {
+	    	string s = hours.substr(i, 2);
+
+	    	hoursArrayS.push_back(hours.substr(i, 2));
+	    }
+
+	    for (int i = 0; i < 3000; i+=3)
+	    {
+	    	tempsArrayS.push_back(temps.substr(i, 2));
+	    }
 	    for (int j= 0; j< hoursArrayS.size(); ++j)
 	    {
-	    	hoursArray[j + offset] = stof(hoursArrayS[j].c_str());
-	    	temperaturesArray[j + offset] = stof(tempsArrayS[j].c_str());
+	    	char* pEnd;
+	    	hoursArray[j + offset] = strtof(hoursArrayS[j].c_str(),&pEnd);
+	    	temperaturesArray[j + offset] = strtof(tempsArrayS[j].c_str(),&pEnd);
 	    	// printf("%i\n", (j + offset));
 	    	// printf("%f\n", hoursArray[j + offset]);
 	    }
@@ -83,25 +103,52 @@ int main(void)
 			/* code */
 	}
 	cudaMemcpy(cudaHours, hoursArray, N*sizeof(float), cudaMemcpyHostToDevice);
-	cudaMemcpy(cudaTemperatures, tempe, N*sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(cudaTemperatures, temperaturesArray, N*sizeof(float), cudaMemcpyHostToDevice);
 	// Statistics calcs
-	float *totalTemps, *totalHours, *vairanceHours, *vairanceTemperature, meanHours, meanTemperature, standardDeviationHours, standardDeviationTemperature;
+	float *totalTemps, *totalHours, *vairanceHours, *vairanceTemperature, *meanHours, *meanTemperature, standardDeviationTemperature;
+	float *totalTempsCuda, *totalHoursCuda, *vairanceHoursCuda, *vairanceTemperatureCuda, *meanHoursCuda, *meanTemperatureCuda;
+	totalTemps = (float*)malloc(1*sizeof(float));
+	totalHours = (float*)malloc(1*sizeof(float));
+	vairanceHours = (float*)malloc(1*sizeof(float));
+	vairanceTemperature = (float*)malloc(1*sizeof(float));
+	meanHours = (float*)malloc(1*sizeof(float));
+	meanTemperature = (float*)malloc(1*sizeof(float));
+	cudaMalloc(&totalTempsCuda, 1*sizeof(float));
+	cudaMalloc(&totalHoursCuda, 1*sizeof(float));
+	cudaMalloc(&vairanceHoursCuda, 1*sizeof(float));
+	cudaMalloc(&vairanceTemperatureCuda, 1*sizeof(float));
+	cudaMalloc(&meanHoursCuda, 1*sizeof(float));
+	cudaMalloc(&meanTemperatureCuda, 1*sizeof(float));
+	// totalHours = 0.0f;
+	// totalTemps = 0.0f;
+	// vairanceHours = 0.0f;
+	// vairanceTemperature = 0.0f;
+	getTotal<<<hoursArrayS.size()/4, 4>>>(hoursArrayS.size(), cudaHours, totalHoursCuda);
+	getTotal<<<hoursArrayS.size()/4, 4>>>(hoursArrayS.size(), cudaTemperatures, totalTempsCuda);
 
-	getTotal<<<N/4, 4>>>(N, cudaHours, totalHours);
-	getTotal<<<N/4, 4>>>(N, cudaTemperatures, totalTemps);
 	cudaDeviceSynchronize();
-	meanTemperature = totalTemps/N;
-	meanHours = totalHours/N;
-	getVariance<<<N/4, 4>>>(N, cudaHours, meanHours, vairanceHours);
-	getVariance<<<N/4, 4>>>(N, cudaTemperatures, meanTemperature, vairanceTemperature);
+	cudaMemcpy(totalHours, totalHoursCuda, 1*sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(totalTemps, totalTempsCuda, 1*sizeof(float), cudaMemcpyHostToDevice);
+	printf("%f\n", totalHours[0]);
+	meanTemperature[0] = totalTemps[0]/N;
+	meanHours[0] = totalHours[0]/N;
+	cudaMemcpy(meanTemperatureCuda, meanTemperature, 1*sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(meanHoursCuda, meanHours, 1*sizeof(float), cudaMemcpyHostToDevice);
+	getVariance<<<hoursArrayS.size()/4, 4>>>(hoursArrayS.size(), cudaHours, meanHoursCuda, vairanceHoursCuda);
+	getVariance<<<hoursArrayS.size()/4, 4>>>(hoursArrayS.size(), cudaTemperatures, meanTemperatureCuda, vairanceTemperatureCuda);
+
 	cudaDeviceSynchronize();
-	vairanceHours = vairanceHours/N;
-	vairanceTemperature = vairanceTemperature/N;
-	printf("La desviacion estandar de la temperatura entre las 13:00 - 16:00 fue de %f\n", vairanceTemperature);
-	printf("La media de la temperatura entre las 13:00 - 16:00 fue de %f\n", totalTemps);
-    for (int j= 0; j< N; ++j)
-    {
-    	printf("%f\n", hoursArray[j]);
-    	printf("%f\n", temperaturesArray[j]);
-    }
+	cudaMemcpy(vairanceHoursCuda, vairanceHours, 1*sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(vairanceTemperatureCuda, vairanceTemperature, 1*sizeof(float), cudaMemcpyHostToDevice);
+	vairanceHours[0] = vairanceHours[0]/hoursArrayS.size();
+	vairanceTemperature[0] = vairanceTemperature[0]/hoursArrayS.size();
+	standardDeviationTemperature = sqrt(vairanceTemperature[0]);
+	printf("La desviacion estandar de la temperatura entre las 13:00 - 16:00 fue de %f\n", standardDeviationTemperature);
+	printf("La varianza de la temperatura entre las 13:00 - 16:00 fue de %f\n", vairanceTemperature[0]);
+	printf("La media de la temperatura entre las 13:00 - 16:00 fue de %f\n", meanTemperature[0]);
+    // for (int j= 0; j< N; ++j)
+    // {
+    // 	printf("%f\n", hoursArray[j]);
+    // 	printf("%f\n", temperaturesArray[j]);
+    // }
 }
